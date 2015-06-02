9ffb67884b00c9c50d202249b610d503cf565832
Enis Solakoglu <enisslkglu@gmail.com>
2015-06-02 18:02:19 +0300

    1.Persons

---
+++ 01.Persons/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+
+class Person
+{
+
+    private string name;
+    private int age;
+    private string email;
+
+    public Person(string name, int age, string email)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Email = email;
+    }
+
+    public Person()
+    {
+        this.Name = null;
+        this.Age = 0;
+        this.Email = null;
+    }
+    
+    public string Name
+    {
+
+        get { return this.name; }
+        set { if (value != null) this.name = CheckForThrow(value); }
+    }
+
+    private string CheckForThrow(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new ArgumentNullException("Name","Name cannot be null or empty!!!");
+        }
+        return value;
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set
+        {
+            if (value > 100 || value < 0)
+            {
+                throw new ArgumentOutOfRangeException("Age cannot be smaller than \"0" + "\" or bigger than \"100\"");
+            }
+            this.age = value;
+        }
+    }
+
+    public string Email
+    {
+        get { return this.email; }
+        set { this.email = value; }
+    }
+    
+
+    public override string ToString()
+    {
+        if (Email.Contains("@"))
+        {
+            return string.Format("Name: {0}\nAge: {1}\nEmail: {2}", this.Name, this.Age, this.Email);
+        }
+        else
+        {
+            return string.Format("Name: {0}\nAge: {1}", this.Name, this.Age);
+        }
+    }
+}
+
---
+++ 01.Persons/_Persons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Persons
+{
+    static void Main()
+    {
+        var infos = new Person
+        {
+            Name = Console.ReadLine(),
+            Age = int.Parse(Console.ReadLine()),
+            Email = Console.ReadLine()
+        };
+        
+        Console.WriteLine(infos);
+
+        
+    }
+    
+}
+
