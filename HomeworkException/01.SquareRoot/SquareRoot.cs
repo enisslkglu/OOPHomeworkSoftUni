7f05415a4d49c0369b05cf80a4bc005c7b2938d1
Enis Solakoglu <enisslkglu@gmail.com>
2015-06-17 15:57:27 +0300

    Exception

---
+++ HomeworkException/01.SquareRoot/SquareRoot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            string num = Console.ReadLine();
+            try
+            {
+                int numbers = int.Parse(num);
+                if (numbers < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Number cannot be negative!!!");
+                }
+                int result = (int) Math.Sqrt(numbers);
+                Console.WriteLine("Result is {0}", result);
+            }
+            catch (ArgumentOutOfRangeException are)
+            {
+                Console.Error.WriteLine("Invalid input: " + are.Message);
+                throw;
+            }
+            catch (FormatException fe)
+            {
+                Console.Error.WriteLine("Invalid format: " + fe.Message);
+            }
+            catch (OverflowException oe)
+            {
+                Console.Error.WriteLine("Input is upper than MaxInt32: " + oe.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\nGood bye!");
+            }
+        }
+
+    }
+}
---
+++ HomeworkException/02.EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.EnterNumbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            List<int> nums = new List<int>();
+            
+            while (nums.Count < 10)
+            {
+                nums.Add(ReadNumbers(1, 100));
+            }
+            nums.Sort();
+            Console.WriteLine("Numbers;");
+            Console.WriteLine(string.Join(", ", nums));
+            
+        }
+
+        private static int ReadNumbers(int start, int end)
+        {
+            int numbers;
+            while (true)
+            {
+                try
+                {
+                    numbers = int.Parse(Console.ReadLine());
+                    if (numbers <= start || numbers >= end)
+                    {
+                        throw new ArgumentOutOfRangeException("Number must be between 0 and 100!!!");
+                    }
+                    return numbers;
+                }
+                catch (ArgumentOutOfRangeException are)
+                {
+                    Console.Error.WriteLine("Input must be between \"0\" and \"100\"!" + are.Message);
+                }
+                catch (FormatException fe)
+                {
+                    Console.Error.WriteLine("Invalid format!" + fe.Message);
+                }
+                
+            }
+            
+        }
+    }
+}
